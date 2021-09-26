c54fa50460d007cd1621e488356f91eef88066b8
Igor Kachurin <ikachurin@dataarmor.ru>
2021-09-26 19:06:31 +0300

    fix indents to from spaces to tabs

--- src/is_arg_valid.c
+++ src/is_arg_valid.c
@@ -1,65 +1,65 @@
 #include "../includes/push_swap.h"
 
-static bool is_minus_with_number(const char* str)
+static bool is_minus_with_number(const char *str)
 {
-    char current_symbol;
-    char next_symbol;
+	char current_symbol;
+	char next_symbol;
 
-    current_symbol = *str;
-    next_symbol = *(str + 1);
-    if (current_symbol == '-' && ft_isdigit(next_symbol))
-        return true;
-    return false;
+	current_symbol = *str;
+	next_symbol = *(str + 1);
+	if (current_symbol == '-' && ft_isdigit(next_symbol))
+		return true;
+	return false;
 }
 
 static bool is_arg_contains_only_integers(char **argv)
 {
-    char *temp;
-    long long number;
+	char *temp;
+	long long number;
 
-    while (*argv)
-    {
-        temp = *argv;
-        while (**argv)
-        {
-            if (ft_isdigit(**argv) || is_minus_with_number(*argv))
-                (*argv)++;
-            else
-                return false;
-        }
-        *argv = temp;
-        number = ft_atoll(*argv);
-        if (number > INT_MAX || number < INT_MIN)
-            return false;
-        argv++;
-    }
-    return true;
+	while (*argv)
+	{
+		temp = *argv;
+		while (**argv)
+		{
+			if (ft_isdigit(**argv) || is_minus_with_number(*argv))
+				(*argv)++;
+			else
+				return false;
+		}
+		*argv = temp;
+		number = ft_atoll(*argv);
+		if (number > INT_MAX || number < INT_MIN)
+			return false;
+		argv++;
+	}
+	return true;
 }
 
 static bool is_arg_without_duplicates(char **argv)
 {
-    char **current;
-    char **comparing;
+	char **current;
+	char **comparing;
 
-    current = argv;
-    while (*current)
-    {
-        comparing = ++argv;
-        while (*comparing)
-        {
-            if (ft_strcmp(*current, *comparing) == 0)
-                return false;
-            comparing++;
-        }
-        current = argv;
-    }
-    return true;
+	current = argv;
+	while (*current)
+	{
+		comparing = ++argv;
+		while (*comparing)
+		{
+			if (ft_strcmp(*current, *comparing) == 0)
+				return false;
+			comparing++;
+		}
+		current = argv;
+	}
+	return true;
 }
 
 bool is_arg_valid(char **argv)
 {
-    if (is_arg_without_duplicates(argv)
-        && is_arg_contains_only_integers(argv))
-        return true;
-    return false;
+	if (is_arg_without_duplicates(argv)
+		&& is_arg_contains_only_integers(argv))
+		return true;
+	return false;
 }
--- src/push_swap.c
+++ src/push_swap.c
@@ -2,67 +2,68 @@
 
 bool add_element_to_stack(char *str, t_list **a_stack)
 {
-    int *number;
-    t_list *element;
+	int *number;
+	t_list *element;
 
-    number = malloc(sizeof(int));
-    if (number == NULL)
-        return false;
-    *number = ft_atoi(str);
-    element = ft_lstnew(number);
-    if (element == NULL)
-    {
-        free(number);
-        return false;
-    }
-    ft_lstadd_back(a_stack, element);
-    return true;
+	number = malloc(sizeof(int));
+	if (number == NULL)
+		return false;
+	*number = ft_atoi(str);
+	element = ft_lstnew(number);
+	if (element == NULL)
+	{
+		free(number);
+		return false;
+	}
+	ft_lstadd_back(a_stack, element);
+	return true;
 }
 
-bool    fill_stack(t_list **a_stack, char **argv)
+bool fill_stack(t_list **a_stack, char **argv)
 {
-    *a_stack = 0;
-    while (*argv)
-    {
-        if (add_element_to_stack(*argv, a_stack) == false)
-        {
-            ft_lstclear(a_stack, free);
-            return false;
-        }
-        argv++;
-    }
-    return true;
+	*a_stack = 0;
+	while (*argv)
+	{
+		if (add_element_to_stack(*argv, a_stack) == false)
+		{
+			ft_lstclear(a_stack, free);
+			return false;
+		}
+		argv++;
+	}
+	return true;
 }
 
 int compare_stack_content(void *first, void *second)
 {
-    int *int_first;
-    int *int_second;
+	int *int_first;
+	int *int_second;
 
-    int_first = (int *)first;
-    int_second = (int *)second;
-    return *int_first - *int_second;
+	int_first = (int *) first;
+	int_second = (int *) second;
+	return *int_first - *int_second;
 }
 
 int main(int argc, char **argv)
 {
-    t_list *a_stack;
-    t_list *temp;
+	t_list *a_stack;
+	t_list *temp;
 
-    argv++;
-    if (argc == 1 || !is_arg_valid(argv) || !fill_stack(&a_stack, argv))
-    {
-        ft_putstr_fd(ERROR_MESSAGE, STDOUT_FILENO);
-        return 1;
-    }
-    temp = a_stack;
+	argv++;
+	if (argc == 1 || !is_arg_valid(argv) || !fill_stack(&a_stack, argv))
+	{
+		ft_putstr_fd(ERROR_MESSAGE, STDOUT_FILENO);
+		return 1;
+	}
+	temp = a_stack;
 
-    ft_lstsort(&a_stack, compare_stack_content);
-    int size = ft_lstsize(a_stack);
-    for (int i = 0; i < size; ++i) {
-        int number = *(int *)a_stack->content;
-        printf("%d ", number);
-        a_stack = a_stack->next;
-    }
-    ft_lstclear(&temp, free);
+	ft_lstsort(&a_stack, compare_stack_content);
+	int size = ft_lstsize(a_stack);
+	for (int i = 0; i < size; ++i)
+	{
+		int number = *(int *) a_stack->content;
+		printf("%d ", number);
+		a_stack = a_stack->next;
+	}
+	ft_lstclear(&temp, free);
 }
