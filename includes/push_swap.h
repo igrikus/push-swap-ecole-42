0b2b56843cb6c19b039ec0008fd6bfcb10c7f924
Igor Kachurin <ikachurin@dataarmor.ru>
2021-10-09 22:26:37 +0300

    norminette

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -1,34 +1,38 @@
 #ifndef PUSH_SWAP_H
-#define PUSH_SWAP_H
+# define PUSH_SWAP_H
 
-#include <limits.h>
-#include <stdbool.h>
-#include "../libft/libft.h"
+# include <limits.h>
+# include <stdbool.h>
+# include "../libft/libft.h"
 
-#define ERROR_MESSAGE "Error"
+# define ERROR_MESSAGE "Error"
 
-bool is_arg_valid(char **argv);
-void push_a(t_list **a_stack, t_list **b_stack);
-void push_b(t_list **a_stack, t_list **b_stack);
-void swap_a(t_list **a_stack);
-void swap_b(t_list **b_stack);
-void rotate_a(t_list **a_stack);
-void rotate_b(t_list **b_stack);
-void reverse_rotate_a(t_list **a_stack);
-void reverse_rotate_b(t_list **b_stack);
-void solve(t_list **a_stack, t_list **b_stack);
-void solve_two(t_list **a_stack);
-int	find_mid_value(t_list *stack, int len);
-int get_chunk_size(t_list *stack, int chunk);
-void initialize_chunks(t_list **stack);
-void insert_chunk_number_len(t_list **stack, int chunk, int len);
-void pull_two_to_b(t_list **a_stack, t_list **b_stack);
-void pull_two_to_a(t_list **a_stack, t_list **b_stack);
+bool	is_arg_valid(char **argv);
 
-void pull_chunk_to_b(t_list **a_stack, t_list **b_stack, int *chunk, int pulled_chunk);
-void pull_chunk_to_a(t_list **a_stack, t_list **b_stack, int *chunk, int pulled_chunk);
+void	push_a(t_list **a_stack, t_list **b_stack);
+void	push_b(t_list **a_stack, t_list **b_stack);
+void	swap_a(t_list **a_stack);
+void	swap_b(t_list **b_stack);
 
-void print_stack(t_list *stack);
-void print_chunk(t_list *stack, int chunk);
+void	rotate_a(t_list **a_stack);
+void	rotate_b(t_list **b_stack);
+void	reverse_rotate_a(t_list **a_stack);
+void	reverse_rotate_b(t_list **b_stack);
+
+void	solve(t_list **a_stack, t_list **b_stack);
+void	solve_two(t_list **a_stack);
+
+int		find_mid_value(t_list *stack, int len);
+
+int		get_chunk_size(t_list *stack, int chunk);
+void	initialize_chunks(t_list **stack);
+void	insert_chunk_number_len(t_list **stack, int chunk, int len);
+void	pull_two_to_b(t_list **a_stack, t_list **b_stack);
+void	pull_two_to_a(t_list **a_stack, t_list **b_stack);
+
+void	pull_chunk_to_b(t_list **a_stack, t_list **b_stack, int *chunk,
+			int pulled_chunk);
+void	pull_chunk_to_a(t_list **a_stack, t_list **b_stack, int *chunk,
+			int pulled_chunk);
 
 #endif
--- includes/wrappers.h
+++ includes/wrappers.h
@@ -1,19 +1,19 @@
 #ifndef WRAPPERS_H
-#define WRAPPERS_H
+# define WRAPPERS_H
 
-#include "../libft/libft.h"
+# include "../libft/libft.h"
 
-#define SWAP_A "sa"
-#define SWAP_B "sb"
-#define PUSH_A "pa"
-#define PUSH_B "pb"
-#define ROTATE_A "ra"
-#define ROTATE_B "rb"
-#define REVERSE_ROTATE_A "rra"
-#define REVERSE_ROTATE_B "rrb"
+# define SWAP_A "sa"
+# define SWAP_B "sb"
+# define PUSH_A "pa"
+# define PUSH_B "pb"
+# define ROTATE_A "ra"
+# define ROTATE_B "rb"
+# define REVERSE_ROTATE_A "rra"
+# define REVERSE_ROTATE_B "rrb"
 
-void push(t_list **from, t_list **to);
-void swap(t_list **stack);
-void rotate(t_list **stack);
-void reverse_rotate(t_list **stack);
+void	push(t_list **from, t_list **to);
+void	swap(t_list **stack);
+void	rotate(t_list **stack);
+void	reverse_rotate(t_list **stack);
 #endif
